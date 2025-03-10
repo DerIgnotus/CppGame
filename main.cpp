f7c5582f2e7bfdd95ec5e3353ad0a0d62391323a
test <126392031+DerIgnotus@users.noreply.github.com>
2025-03-10 18:40:13 +0100

    VAO, VBO and EBO + Organization

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shaderClass.h"
+#include "VBO.h"
+#include "VAO.h"
+#include "EBO.h"
+
+
 int main()
 {
 	// Initialize GLFW
@@ -11,6 +17,24 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Triangle vertices
+	GLfloat	vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	// Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		// Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,	// Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	// Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	// Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f		// Inner down
+	};
+
+	GLuint indices[] =
+	{
+		0, 3, 5,	// Lower left triangle
+		3, 2, 4,	// Lower right triangle
+		5, 4, 1		// Upper triangle
+	};
 
 	// Create the window
 	// Check if window creating failed
@@ -32,19 +56,54 @@ int main()
 	// Set the viewport
 	glViewport(0, 0, 800, 800);
 
-	// Set background color
-	glClearColor(0.0f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
+	// Create Shaderprogram
+	Shader shaderProgram("default.vert", "default.frag");
+
+	// Create and bind VAO and VBO and EBO
+	VAO VAO1;
+	VAO1.Bind();
+
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkVBO(VBO1, 0);
+
+	// Unbind them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Clear screen
+		glClearColor(0.0f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use shader
+		shaderProgram.Activate();
+
+		// Bind VAO
+		VAO1.Bind();
+
+		// Actually draw the triangles
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+
+
 		// Handle Events
 		glfwPollEvents();
 	}
 
 	// Clean things up
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
